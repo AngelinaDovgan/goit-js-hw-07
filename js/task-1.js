9c16fc42e3afa7d604506679641b79cb89aacab8
AngelinaDovgan <145202232+AngelinaDovgan@users.noreply.github.com>
2024-01-26 11:35:06 +0200

    1 - task

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+const findUl = document.querySelectorAll('ul#categories li.item');
+let countUl = 0;
+findUl.forEach(function(item) {
+    countUl++;
+});
+console.log('Number of categories: ' + countUl);
+
+
+const categoryName = document.querySelectorAll("ul#categories li.item");
+categoryName.forEach(function(el) {
+const title = el.querySelector('h2').textContent;
+const mainTitle = el.querySelectorAll('ul li');
+    console.log('Category: ' + title);
+    console.log('Elements: ' + mainTitle.length);
+});
