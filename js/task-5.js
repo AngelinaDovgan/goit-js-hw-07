4ba9aa5a0b00b23d3a26a5a31ed1d676249ca88b
AngelinaDovgan <145202232+AngelinaDovgan@users.noreply.github.com>
2024-01-26 14:56:02 +0200

    task - 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+
+button.addEventListener('click', () => {
+  // document.body.style.backgroundColor = [getRandomHexColor()];
+  let body = document.querySelector("body");
+  body.style.backgroundColor = [getRandomHexColor()];
+  let colorSpan = document.querySelector(".color");
+  colorSpan.textContent = body.style.backgroundColor;
+});
+
