29aa48c4871d5099c7a0df40b0b89339d4b22514
AngelinaDovgan <145202232+AngelinaDovgan@users.noreply.github.com>
2024-01-26 14:12:30 +0200

    task - 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+function formHandler(event) {
+    event.preventDefault();
+    const emailInput = this.elements.email;
+    const passwordInput = this.elements.password;
 
+    const emailValue = emailInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+    
+    if (!emailValue || !passwordValue) {
+        alert('All form fields must be filled in');
+    } else { 
+        const data = {
+            email: emailValue,
+            password: passwordValue
+        };
+        console.log(data);
+        this.reset();
+    }
+}
+
+form.addEventListener("submit", formHandler);
