2b1021a006fc3d35644a1c387b6c73771d18eff7
AngelinaDovgan <145202232+AngelinaDovgan@users.noreply.github.com>
2024-01-26 17:15:22 +0200

    task - 6

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,5 @@
 const findUl = document.querySelectorAll('ul#categories li.item');
+
 let countUl = 0;
 findUl.forEach(function(item) {
     countUl++;
@@ -7,7 +8,7 @@ console.log('Number of categories: ' + countUl);
 
 
 const categoryName = document.querySelectorAll("ul#categories li.item");
-categoryName.forEach(function(el) {
+categoryName.forEach(function (el) {
 const title = el.querySelector('h2').textContent;
 const mainTitle = el.querySelectorAll('ul li');
     console.log('Category: ' + title);
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const mainDiv = document.querySelector("#controls");
+const input = document.querySelector("input");
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const divBox = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+divBox.innerHTML = '';
+let size = 30;
+for (let i = 0; i < amount; i++) {
+const box = document.createElement("div");
+box.style.width = `${size}px`;
+box.style.height = `${size}px`;
+box.style.backgroundColor = [getRandomHexColor()];
+divBox.append(box);
+size += 10;
+    
+  }
+}
+
+function destroyBoxes() {
+  divBox.innerHTML = '';
+}
+
+createButton.addEventListener("click", function () {
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyButton.addEventListener("click", function () {
+  destroyBoxes();
+});
